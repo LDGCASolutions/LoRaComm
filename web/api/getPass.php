99481af4b1955939f67e7d0a35b8a6a7ec5a77a8
Charith <charithakalanka@live.com>
2022-10-10 23:48:56 +1100

    getPass-add API key check

--- web/api/getPass.php
+++ web/api/getPass.php
@@ -1,19 +1,28 @@
 <?php
 include "../inc/postgresql.inc.php";
 
+$apiKey = "abcdefgHijkLMNOP";
 $nodeID = $_GET["nodeID"];
+
+if ($_GET["apiKey"] != $apiKey) {
+  echo "-1";
+  return;
+}
 
 if (!preg_match("/[A-Z0-9]{4,10}/", $nodeID)) {
   echo "0";
+  return;
+
 } else {
   $query = "SELECT password FROM \"Node\" WHERE ID='".$nodeID."'";
   $query = pg_query($db_connection, $query);
   if (pg_num_rows($query)==0){
     echo "0";
+    return;
   }
   $data = pg_fetch_row($query);
   print_r($data[0]);
+  return;
 }
-
 
 ?>
