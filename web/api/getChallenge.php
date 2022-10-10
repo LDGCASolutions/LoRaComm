96b627f44da8b499cd45bb1a09dc7fe7a4a3c04f
Charith <charithakalanka@live.com>
2022-10-11 09:19:04 +1100

    Retrieve challenge from DB

---
+++ web/api/getChallenge.php
@@ -0,0 +1,30 @@
+<?php
+// Purpose: Retrieves the challenge recorded on ChallengeLog table
+// Require: API key, Node ID
+
+include "../inc/postgresql.inc.php";
+
+$apiKey = "abcdefgHijkLMNOP";
+
+if ($_GET["apiKey"] != $apiKey) {
+  echo "-1";
+  return;
+}
+
+if (!preg_match("/[A-Z0-9]{4,10}/", $_GET["nodeID"])) {
+  echo "0";
+  return;
+}
+
+$query = "SELECT challenge FROM \"ChallengeLog\" WHERE recipient='".$_GET["nodeID"]."'";
+$query = pg_query($db_connection, $query);
+if (pg_num_rows($query)==0){
+  echo "0";
+  return;
+} else {
+  $row = pg_fetch_row($query);
+  echo $row[0];
+  return;
+}
+
+?>
