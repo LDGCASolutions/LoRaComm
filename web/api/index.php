e08a68eecada88ab1c3a71868a36545fd164c957
Charith <charithakalanka@live.com>
2022-10-12 08:34:19 +1100

    Add API usage guide page

---
+++ web/api/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>API Usage Guide</title>
+  </head>
+  <body>
+    <h3>Available endpoints</h3>
+    <p>/authNode.php</p>
+    <p>/getAuthStatus.php</p>
+    <p>/getChallenge.php</p>
+    <p>/getNewChallenge.php</p>
+    <p>/getPass.php</p>
+  </body>
+</html>
